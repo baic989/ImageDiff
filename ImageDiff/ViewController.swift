3cf9f3079da35dd7c0ecd384176027708490ce9c
Hrvoje Baić <hrvoje.baic@greyp.com>
2017-10-22 23:09:13 +0200

    Initial Commit

---
+++ ImageDiff/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageDiff
+//
+//  Created by Super Hrvoje on 22/10/2017.
+//  Copyright © 2017 Hrvoje Baić. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
